32e3c5dc6dc46b9433c8ffad1db281fd3840bfb1
maithileev <maithileev@gmail.com>
2025-05-09 20:33:23 +0200

    Changes to config

--- dashboard/svelte.config.js
+++ dashboard/svelte.config.js
@@ -3,7 +3,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter(
+		{
+			fallback: '404.html'
+		}
+	) }
 };
+
+config.paths = { base: process.argv.includes('dev') ? '' : "/visual-analytics-dashboard" }
 
 export default config;
